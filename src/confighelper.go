356ecb688825109068cb9b89bb9b2046890a5509
Alvin <rebooting@gmail.com>
2018-01-20 08:38:04 +0800

    added json codes

---
+++ src/confighelper.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+type GitConfig struct {
+	NAME   string `json:"name"`
+	GITURL string `json:"giturl"`
+	DESC   string `json:"desc"`
+}
+
+func loadConfigFile(filename string) []GitConfig {
+	raw, err := ioutil.ReadFile(filename)
+	if err != nil {
+		//TODO: handle error
+	}
+	var jsonGitConfig []GitConfig
+	json.Unmarshal(raw, &jsonGitConfig)
+	return jsonGitConfig
+}
